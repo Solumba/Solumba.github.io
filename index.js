6e476c1c1bf7e1cdbc86dfcb9e459d30d2083903
Olumba Solomon <61973841+Solumba@users.noreply.github.com>
2021-04-08 19:38:08 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const burger = document.getElementById("burger");
-const navLinks = document.querySelector('.nav-links');
-const navs = document.getElementById('navs');
-console.log(navLinks);
-
-burger.addEventListener('click', ()=>{
-    navs.classList.toggle('toggler')
-    burger.classList.toggle('toggle');
-});
